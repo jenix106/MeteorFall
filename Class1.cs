2e7e89116f272bbf8b09bc0f361314bc2336ba0b
Ian Hooey <ian.hooey@gmail.com>
2023-03-05 17:51:33 -0600

    Updated

--- Class1.cs
+++ Class1.cs
@@ -5,6 +5,22 @@ using UnityEngine;
 
 namespace MeteorFall
 {
+    public class MeteorSpell : SpellCastProjectile
+    {
+        public override void Fire(bool active)
+        {
+            base.Fire(active);
+            if (active)
+            {
+                spellCaster.SetMagicOffset(new Vector3(-0.04f, 0.2f, -0.07f));
+            }
+        }
+        public override void Throw(Vector3 velocity)
+        {
+            base.Throw(velocity);
+            guidedProjectile.item.GetComponent<MeteorProjectileComponent>().creature = spellCaster.mana.creature;
+        }
+    }
     public class MeteorProjectile : ItemModule
     {
         public override void OnItemLoaded(Item item)
@@ -16,10 +32,12 @@ namespace MeteorFall
     public class MeteorProjectileComponent : MonoBehaviour
     {
         Item item;
+        public Creature creature;
         public void Start()
         {
             item = GetComponent<Item>();
             item.mainCollisionHandler.OnCollisionStartEvent += MainCollisionHandler_OnCollisionStartEvent;
+            item.disallowDespawn = true;
         }
 
         private void MainCollisionHandler_OnCollisionStartEvent(CollisionInstance collisionInstance)
@@ -27,46 +45,48 @@ namespace MeteorFall
             collisionInstance.ignoreDamage = true;
             Catalog.GetData<ItemData>("MeteorFall").SpawnAsync(meteor =>
             {
-                meteor.gameObject.AddComponent<MeteorComponent>();
+                meteor.gameObject.AddComponent<MeteorComponent>().creature = creature;
                 //meteor.transform.position = new Vector3(collisionInstance.contactPoint.x, collisionInstance.contactPoint.y + 300, collisionInstance.contactPoint.z);
-                meteor.Throw(20);
-            }, new Vector3(collisionInstance.contactPoint.x, collisionInstance.contactPoint.y + 200, collisionInstance.contactPoint.z));
+                meteor.rb.AddForce(Vector3.down * 500, ForceMode.VelocityChange);
+                meteor.Throw();
+            }, new Vector3(collisionInstance.contactPoint.x, collisionInstance.contactPoint.y + 2000, collisionInstance.contactPoint.z));
         }
     }
     public class MeteorComponent : MonoBehaviour
     {
         Item item;
-        EffectInstance instance;
-        public void Start()
+        public Creature creature;
+        public void Awake()
         {
             item = GetComponent<Item>();
             item.mainCollisionHandler.OnCollisionStartEvent += MainCollisionHandler_OnCollisionStartEvent;
-            instance = Catalog.GetData<EffectData>("ImbueFire").Spawn(item.transform, true);
-            instance.SetRenderer(item.renderers[0], false);
-            instance.SetIntensity(50);
-            instance.Play();
+            item.disallowDespawn = true;
         }
         private void MainCollisionHandler_OnCollisionStartEvent(CollisionInstance collisionInstance)
         {
             StartCoroutine(Impact(collisionInstance.contactPoint, collisionInstance.contactNormal, collisionInstance.sourceColliderGroup.transform.up));
-            instance.Stop();
+            item.Hide(true);
+            item.colliderGroups[0].colliders[0].isTrigger = true;
+            item.rb.velocity = Vector3.zero;
+            item.rb.Sleep();
+            item.disallowDespawn = false;
             item.mainCollisionHandler.OnCollisionStartEvent -= MainCollisionHandler_OnCollisionStartEvent;
         }
         private IEnumerator Impact(Vector3 contactPoint, Vector3 contactNormal, Vector3 contactNormalUpward)
         {
             EffectInstance effectInstance = Catalog.GetData<EffectData>("MeteorShockwave").Spawn(contactPoint, Quaternion.LookRotation(-contactNormal, contactNormalUpward));
-            effectInstance.SetIntensity(100);
+            effectInstance.SetIntensity(100f);
             effectInstance.Play();
             Collider[] sphereContacts = Physics.OverlapSphere(contactPoint, 100, 218119169);
             List<Creature> creaturesPushed = new List<Creature>();
             List<Rigidbody> rigidbodiesPushed = new List<Rigidbody>();
             rigidbodiesPushed.Add(item.rb);
-            creaturesPushed.Add(Player.local.creature);
+            creaturesPushed.Add(creature);
             float waveDistance = 0.0f;
             yield return new WaitForEndOfFrame();
             while (waveDistance < 100)
             {
-                waveDistance += 70f * 0.05f;
+                waveDistance += 70f * 0.1f;
                 foreach(Creature creature in Creature.allActive)
                 {
                     if(!creature.isKilled && Vector3.Distance(contactPoint, creature.transform.position) < waveDistance && !creaturesPushed.Contains(creature))
@@ -74,7 +94,8 @@ namespace MeteorFall
                         CollisionInstance collision = new CollisionInstance(new DamageStruct(DamageType.Energy, 200 - (Vector3.Distance(contactPoint, creature.transform.position) * 2)));
                         collision.damageStruct.hitRagdollPart = creature.ragdoll.rootPart;
                         creature.Damage(collision);
-                        creature.TryPush(Creature.PushType.Magic, (creature.ragdoll.rootPart.transform.position - contactPoint).normalized, 3); 
+                        if (!creature.isPlayer)
+                            creature.ragdoll.SetState(Ragdoll.State.Destabilized);
                         if (item?.lastHandler?.creature != null)
                         {
                             creature.lastInteractionTime = Time.time;
@@ -90,14 +111,14 @@ namespace MeteorFall
                     {
                         if (collider.attachedRigidbody.gameObject.layer != GameManager.GetLayer(LayerName.NPC) && !rigidbodiesPushed.Contains(collider.attachedRigidbody))
                         {
-                            collider.attachedRigidbody.AddExplosionForce(50, contactPoint, 100, 0.5f, ForceMode.VelocityChange);
+                            collider.attachedRigidbody.AddExplosionForce(75, contactPoint, 100, 0.5f, ForceMode.VelocityChange);
                             rigidbodiesPushed.Add(collider.attachedRigidbody);
                         }
                     }
                 }
                 yield return new WaitForSeconds(0.05f);
             }
-            Destroy(this);
+            item.Despawn();
         }
     }
     public class MeteorBurning : MonoBehaviour
@@ -105,16 +126,14 @@ namespace MeteorFall
         Creature creature;
         EffectInstance instance;
         float timer;
-        float cooldown;
         public void Start()
         {
             creature = GetComponent<Creature>();
-            instance = Catalog.GetData<EffectData>("MeteorRagdoll").Spawn(creature.transform, true);
+            instance = Catalog.GetData<EffectData>("MeteorRagdoll").Spawn(creature.ragdoll.rootPart.transform, true);
             instance.SetRenderer(creature.GetRendererForVFX(), false);
             instance.SetIntensity(1f);
             instance.Play();
             timer = Time.time;
-            cooldown = Time.time;
         }
         public void FixedUpdate()
         {
@@ -123,11 +142,10 @@ namespace MeteorFall
                 instance.Stop();
                 Destroy(this);
             }
-            else if (Time.time - cooldown >= 1f && !creature.isKilled)
+            else if (!creature.isKilled)
             {
-                CollisionInstance collision = new CollisionInstance(new DamageStruct(DamageType.Energy, 5));
+                CollisionInstance collision = new CollisionInstance(new DamageStruct(DamageType.Energy, 5 * Time.fixedDeltaTime));
                 collision.damageStruct.hitRagdollPart = creature.ragdoll.rootPart;
-                cooldown = Time.time;
                 creature.Damage(collision);
             }
         }
